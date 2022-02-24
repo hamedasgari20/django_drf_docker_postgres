69bd583aaba3128070b55a7164fa5f56ec9ffeea
Hamed Asgari <hamedasgari20@yahoo.com>
2022-02-24 10:59:43 +0330

    add test file to book app

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from book.models import Author
 
-# Create your tests here.
+class AuthorTestCase(TestCase):
+    def setUp(self):
+        Author.objects.create(first_name="Brian", last_name="Tracy")
+        Author.objects.create(first_name="Antony", last_name="Rabins")
+
+
+    def test_author_creation(self):
+        """Author creation test"""
+        brian_tracy = Author.objects.get(first_name="Brian", last_name="Tracy")
+        antony_rabins = Author.objects.get(first_name="Antony", last_name="Rabins")
+        self.assertEqual(brian_tracy, 'Brian Tracy')
+        self.assertEqual(antony_rabins, 'Antony Rabins')
