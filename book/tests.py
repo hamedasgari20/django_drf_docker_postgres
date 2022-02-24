74dd43540eae62d5e16ef9b9b6b6d11ec6a05f6e
Hamed Asgari <hamedasgari20@yahoo.com>
2022-02-24 11:42:46 +0330

    book/tests.py has been modified

--- book/tests.py
+++ book/tests.py
@@ -4,12 +4,11 @@ from book.models import Author
 class AuthorTestCase(TestCase):
     def setUp(self):
         Author.objects.create(first_name="Brian", last_name="Tracy")
-        Author.objects.create(first_name="Antony", last_name="Rabins")
-
 
     def test_author_creation(self):
         """Author creation test"""
-        brian_tracy = Author.objects.get(first_name="Brian", last_name="Tracy")
-        antony_rabins = Author.objects.get(first_name="Antony", last_name="Rabins")
-        self.assertEqual(brian_tracy, 'Brian Tracy')
-        self.assertEqual(antony_rabins, 'Antony Rabins')
+        first_name = Author.objects.get(first_name="Brian").first_name
+        last_name = Author.objects.get(first_name="Brian").last_name
+
+        self.assertEqual(first_name, 'Brian')
+        self.assertEqual(last_name, "Tracy")
